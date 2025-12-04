10c3fa30c5b9f032c63388f336f0f6e02087314e
Emir <muhamademiralfani@gmail.com>
2025-12-04 21:51:21 +0700

    add portfolio jurnalistik

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,5 +1,3 @@
-// src/data/portfolioData.js
-
 export const portfolioData = {
   jurnalistik: {
     title: "Jurnalistik",
@@ -8,7 +6,7 @@ export const portfolioData = {
       {
         id: 1,
         title: "Bintang Radio Mengajarkan Generasi Muda Boyong Warisan Musik Daerah",
-        image: "https://www.jabaronline.com/media/images/2025/11/11690928d1473fe.jpeg?location=14&width=&height=&quality=90&fit=1", // Placeholder musik/radio
+        image: "https://www.jabaronline.com/media/images/2025/11/11690928d1473fe.jpeg?location=14&width=&height=&quality=90&fit=1",
         date: "2025",
         excerpt: "Liputan mengenai ajang Bintang Radio yang menjadi wadah pelestarian musik daerah bagi generasi muda.",
         link: "https://www.jabaronline.com/bintang-radio-mengajarkan-generasi-muda-boyong-warisan-musik-daerah"
@@ -16,7 +14,7 @@ export const portfolioData = {
       {
         id: 2,
         title: "Persaingan Sengit Bintang Radio 2025: Voting Online Memanas",
-        image: "https://www.jabaronline.com/media/images/2025/11/1169095eb14a4ce.jpeg?location=14&width=&height=&quality=90&fit=1", // Placeholder voting/kompetisi
+        image: "https://www.jabaronline.com/media/images/2025/11/1169095eb14a4ce.jpeg?location=14&width=&height=&quality=90&fit=1",
         date: "2025",
         excerpt: "Berita terkini mengenai antusiasme publik dalam voting online ajang Bintang Radio 2025.",
         link: "https://www.jabaronline.com/persaingan-sengit-bintang-radio-2025-voting-online-memanas"
@@ -24,10 +22,26 @@ export const portfolioData = {
       {
         id: 3,
         title: "Bintang Radio 2025: Persaingan Sengit Talenta Muda di Panggung Jakarta",
-        image: "https://www.jabaronline.com/media/images/2025/11/116909c6b5b7ecf.jpeg?location=14&width=&height=&quality=90&fit=1", // Placeholder panggung
+        image: "https://www.jabaronline.com/media/images/2025/11/116909c6b5b7ecf.jpeg?location=14&width=&height=&quality=90&fit=1",
         date: "2025",
         excerpt: "Reportase langsung dari panggung Jakarta mengenai kompetisi talenta muda.",
         link: "https://www.jabaronline.com/bintang-radio-2025-persaingan-sengit-talenta-muda-di-panggung-jakarta"
+      },
+      {
+        id: 4,
+        title: "YWKI Luncurkan Gerakan 'Hidup Sesudah Hidup' untuk Kauny Al-Quran University",
+        image: "https://www.jabaronline.com/media/images/2025/10/1168feea7c85477.jpeg?location=14&width=&height=&quality=90&fit=1", // Placeholder (bisa diganti dengan URL gambar asli dari berita)
+        date: "Terkini",
+        excerpt: "Peluncuran gerakan wakaf produktif oleh YWKI sebagai upaya pembangunan fasilitas pendidikan Kauny Al-Quran University.",
+        link: "https://www.jabaronline.com/ywki-luncurkan-gerakan-hidup-sesudah-hidup-gerakan-wakaf-produktif-untuk-pembangunan-kauny-al-quran-university"
+      },
+      {
+        id: 5,
+        title: "Talkshow Music Prenur: Kemkraf & RRI Dukung Pelestarian Musik Lokal",
+        image: "https://www.jabaronline.com/media/images/2025/03/1167c92c85de523.jpeg?location=14&width=&height=&quality=90&fit=1", // Placeholder (bisa diganti dengan URL gambar asli dari berita)
+        date: "Terkini",
+        excerpt: "Liputan kolaborasi antara Kemenparekraf dan RRI dalam mendukung pengembangan serta pelestarian musik lokal melalui talkshow interaktif.",
+        link: "https://www.jabaronline.com/talkshow-music-prenur-kemkraf-bekerjasama-rri-mendukung-plestarian-dan-pengembangan-music-lokal"
       },
     ]
   },
